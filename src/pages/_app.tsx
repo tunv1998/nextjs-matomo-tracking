50fbc38d0d55f169c97b0f990e33e0b20eb0427f
David Nguyen <nguyenvantu191298@gmail.com>
2023-03-06 15:34:59 +0700

    add tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
@@ -9,11 +9,15 @@ import WithSubnavigation from "@/modules/layouts/header";
 
 import { init } from "@socialgouv/matomo-next";
 
-const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL ?? "";
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID ?? "4";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    init({ siteId: MATOMO_SITE_ID, url: MATOMO_URL });
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
